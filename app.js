4c18b53b74323ef583c0c035e5a79947dc02ea29
Shiguruji <80973804+Shiguruji@users.noreply.github.com>
2021-06-28 00:52:49 +0100

    added menu list for mobile screens

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+const navLinks = document.getElementById("nav-links");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+ 
