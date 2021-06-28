6787db88b1aaf79a98040a97b68e3c5f131a15e0
Shiguruji <80973804+Shiguruji@users.noreply.github.com>
2021-06-28 11:32:03 +0100

    fixed menu links scroll

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const menu = document.getElementById("menu");
 const navLinks = document.getElementById("nav-links");
 const icon = document.getElementById("test");
+const erase = document.getElementById("try");
+const erase2 = document.getElementById("btn-cont")
 
 menu.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   icon.classList.toggle("highlight");
+  erase.classList.toggle("wipe");
+  erase2.classList.toggle("wipe");
 });
 
 
