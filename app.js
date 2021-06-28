439ed79571f98d7be2ebc75ae6d360383c757cfe
Shiguruji <80973804+Shiguruji@users.noreply.github.com>
2021-06-28 09:56:27 +0100

    added menu highlight

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const menu = document.getElementById("menu");
 const navLinks = document.getElementById("nav-links");
+// const icon = document.getElementById("test");
 
 menu.addEventListener("click", () => {
   navLinks.classList.toggle("show");
+  // icon.classList.toggle("highlight");
 });
 
 
